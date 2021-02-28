a7ef85c192ec23508554066c00fb98a9f77214a4
vladimirsluhaj <sluhayv@gmail.com>
2021-02-28 15:59:41 +0200

    решил задачу ДЗ 4 задача 1. По вашему примкеру в видео, пробовал разбирался и по немного другому. Если второй вариант не правильный, подскажите почему (не уверен, но пытался) с 2-й задачей воюю, есть сложности. Пока не сдаю

---
+++ HW_4.py
@@ -0,0 +1,28 @@
+# это япробовал по Вашему методу
+# a = 7
+# b = '1'
+# try:
+#     print(a/b)
+# except:
+#     if b == 0:
+#         print('ZeroDivisionError: ', ZeroDivisionError)
+#     else:
+#         print ('TypeError: ', TypeError)
+
+#  это я пробовал по своему
+# try:
+#     print(5/1)
+# except(ZeroDivisionError):
+#     print('Деление на 0 недопустимо')
+# except(TypeError):
+#     print('TypeError')
+
+# cars = [{"brand": "ford", "model": "MusTAng Gt500", "year": 2018},
+#         {"brand": "ZAZ", "model": "Fortza", "year": 2001},
+#         {"brand": "VW", "model": "Golf GTI", "year": 1999}
+# ]
+# cars = []
+# print('cars_dict: ', cars_dict)
+# # empty_dict = {}
+# # print('empty_dict: ', empty_dict)
+# # print('empty_dict: ',type(empty_dict))
